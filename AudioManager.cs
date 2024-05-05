864649bbf508d0b9c3a56741c2f68727fb2aa092
Coloride <108619637+realcoloride@users.noreply.github.com>
2024-05-05 02:58:59 +0200

    Add project files.

---
+++ AudioManager.cs
@@ -0,0 +1,135 @@
+﻿using NAudio.Wave;
+using OpenVoiceSharp;
+using Steamworks;
+using SharpAudio;
+using System.Diagnostics;
+
+namespace OpenVoiceSharpSteamworks
+{
+    public enum PlaybackBackend
+    {
+        NAudio,
+        SharpAudio
+    }
+
+    public static class AudioManager
+    {
+        public static PlaybackBackend PlaybackBackend = PlaybackBackend.SharpAudio;
+
+        #region NAudio
+
+        public static WaveFormat WaveFormat = new(VoiceChatInterface.SampleRate, 16, 1); // mono 16 bit 48kHz
+
+        private static Dictionary<SteamId, (BufferedWaveProvider, DirectSoundOut)> WaveOuts = new();
+
+        private static void CreateWaveOut(SteamId steamId)
+        {
+            if (WaveOuts.ContainsKey(steamId)) return;
+
+            BufferedWaveProvider bufferedWaveProvider = new(WaveFormat);
+            DirectSoundOut waveOut = new();
+
+            bufferedWaveProvider.DiscardOnBufferOverflow = true;
+            bufferedWaveProvider.ReadFully = true;
+
+            waveOut.Init(bufferedWaveProvider);
+            waveOut.Play();
+
+            WaveOuts.TryAdd(steamId, new(bufferedWaveProvider, waveOut));
+        }
+
+        private static (BufferedWaveProvider, DirectSoundOut) GetWaveOut(SteamId steamId) => WaveOuts[steamId];
+
+        #endregion NAudio
+
+        #region SharpAudio
+
+        public static AudioEngine AudioEngine = AudioEngine.CreateDefault();
+        public static AudioFormat AudioFormat = new()
+        {
+            SampleRate = VoiceChatInterface.SampleRate,
+            Channels = 1,
+            BitsPerSample = 16
+        };
+
+        private static Dictionary<SteamId, (AudioSource, AudioBuffer)> AudioSources = new();
+
+        private static void CreateAudioSource(SteamId steamId)
+        {
+            if (AudioSources.ContainsKey(steamId)) return;
+
+            AudioBuffer audioBuffer = AudioEngine.CreateBuffer();
+            AudioSource audioSource = AudioEngine.CreateSource();
+
+            AudioSources.TryAdd(steamId, new(audioSource, audioBuffer));
+        }
+
+        private static (AudioSource, AudioBuffer) GetAudioSource(SteamId steamId) => AudioSources[steamId];
+
+        #endregion SharpAudio
+
+        #region Common
+        public static (dynamic, dynamic) GetAudioPlayback(SteamId steamId)
+        {
+            return PlaybackBackend switch
+            {
+                PlaybackBackend.NAudio => GetWaveOut(steamId),
+                PlaybackBackend.SharpAudio => GetAudioSource(steamId),
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        public static void CreateAudioPlayback(SteamId steamId)
+        {
+            switch (PlaybackBackend)
+            {
+                case PlaybackBackend.NAudio: CreateWaveOut(steamId); break;
+                case PlaybackBackend.SharpAudio: CreateAudioSource(steamId); break;
+            }
+        }
+        public static void RemoveAudioPlayback(SteamId steamId)
+        {
+            switch (PlaybackBackend)
+            {
+                case PlaybackBackend.NAudio: WaveOuts.Remove(steamId); break;
+                case PlaybackBackend.SharpAudio: AudioSources.Remove(steamId); break;
+            }
+        }
+        public static bool DoesPlaybackExist(SteamId steamId)
+        {
+            return PlaybackBackend switch
+            {
+                PlaybackBackend.NAudio => WaveOuts.ContainsKey(steamId),
+                PlaybackBackend.SharpAudio => AudioSources.ContainsKey(steamId),
+                _ => false,
+            };
+        }
+        public static void QueueDataForPlayback(SteamId steamId, byte[] data, int length)
+        {
+            switch (PlaybackBackend)
+            {
+                case PlaybackBackend.NAudio:
+                    // get playback and queue samples
+                    var (bufferedWaveProvider, _) = GetAudioPlayback(steamId);
+
+                    // add samples
+                    bufferedWaveProvider.AddSamples(data, 0, length);
+                    break;
+                case PlaybackBackend.SharpAudio:
+                    // get source and queue samples
+                    var (audioSource, audioBuffer) = GetAudioPlayback(steamId);
+
+                    // queue samples
+                    audioBuffer.BufferData(data, AudioFormat);
+                    audioSource.QueueBuffer(audioBuffer);
+
+                    // play
+                    audioSource.Play();
+
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using OpenVoiceSharp;
+using OpenVoiceSharpSteamworks;
+using SharpAudio;
+using Steamworks;
+using Steamworks.Data;
+
+// play back?
+const bool DisableLoopback = false;
+
+Lobby? lobby = null;
+SteamClient.Init(480);
+
+// steam
+// store playback naudio objects
+SteamMatchmaking.OnLobbyMemberJoined += (lobby, friend) =>
+{
+    if (friend.Id == SteamClient.SteamId) return;
+
+    Console.WriteLine($"{friend.Name} requests to join");
+    SteamNetworking.AcceptP2PSessionWithUser(friend.Id);
+
+    Console.WriteLine($"{friend.Name} joined");
+    AudioManager.CreateAudioPlayback(friend.Id);
+};
+SteamMatchmaking.OnLobbyMemberLeave += (lobby, friend) =>
+{
+    if (!AudioManager.DoesPlaybackExist(friend.Id)) return;
+
+    Console.WriteLine($"{friend.Name} left");
+    // clean garbage
+    var (bufferedWaveProvider, waveOut) = AudioManager.GetAudioPlayback(friend.Id);
+
+    waveOut.Stop();
+    waveOut.Dispose();
+
+    bufferedWaveProvider.ClearBuffer();
+
+    AudioManager.RemoveAudioPlayback(friend.Id);
+};
+
+void SetupLobby()
+{
+    // get past members and assign them their waveouts
+    foreach (var member in lobby?.Members)
+        AudioManager.CreateAudioPlayback(member.Id);
+}
+SteamMatchmaking.OnLobbyEntered += (joinedLobby) =>
+{
+    Console.WriteLine("Joined lobby");
+
+    // set to current lobby
+    lobby = joinedLobby;
+
+    // setup
+    SetupLobby();
+};
+SteamMatchmaking.OnLobbyCreated += (_, createdLobby) =>
+{
+    Console.WriteLine("Created lobby");
+
+    // set to current lobby
+    lobby = createdLobby;
+
+    // setup
+    SetupLobby();
+};
+
+SteamFriends.OnGameLobbyJoinRequested += async (joinedLobby, friend) =>
+{
+    Console.WriteLine("Accepted invite");
+
+    // set to current lobby
+    lobby = joinedLobby;
+
+    // setup
+    await joinedLobby.Join();
+    SetupLobby();
+};
+
+// voice chat
+VoiceChatInterface voiceChatInterface = new(16000);
+BasicMicrophoneRecorder microphoneRecorder = new();
+
+microphoneRecorder.DataAvailable += (pcmData, length) =>
+{
+    if (lobby == null) return;
+
+    // encode packet
+    (byte[] encodedData, int encodedLength) = voiceChatInterface.SubmitAudioData(pcmData, length);
+
+    // send packet to everyone
+    foreach (var member in lobby?.Members)
+    {
+        AudioManager.CreateAudioPlayback(member.Id);
+        SteamNetworking.SendP2PPacket(member.Id, encodedData, encodedLength, 0, P2PSend.Reliable);
+    }
+};
+
+if (Environment.GetCommandLineArgs().Contains("--host"))
+{
+    var createLobbyOutput = await SteamMatchmaking.CreateLobbyAsync(4);
+    if (createLobbyOutput == null) return;
+    lobby = createLobbyOutput.Value;
+
+    lobby?.SetPublic();
+    lobby?.SetJoinable(true);
+
+    Console.WriteLine("--host specified, creating lobby");
+
+    await lobby?.Join();
+}
+
+microphoneRecorder.StartRecording();
+
+void HandleMessageFrom(SteamId steamId, byte[] data)
+{
+    // avoid receiving messages from self
+    if (DisableLoopback && steamId == SteamClient.SteamId) return;
+
+    (byte[] decodedData, int decodedLength) = voiceChatInterface.WhenDataReceived(data, data.Length);
+    AudioManager.QueueDataForPlayback(steamId, decodedData, decodedLength);
+}
+
+while (true)
+{
+    SteamClient.RunCallbacks();
+
+    while (SteamNetworking.IsP2PPacketAvailable())
+    {
+        var packet = SteamNetworking.ReadP2PPacket();
+        if (!packet.HasValue) continue;
+
+        HandleMessageFrom(packet.Value.SteamId, packet.Value.Data);
+    }
+}
